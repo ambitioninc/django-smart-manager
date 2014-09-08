5498bbffe6452d5db5916b6ac65aa70ae47f0c5a
Wes Kendall <wesleykendall@gmail.com>
2014-09-08 09:55:00 -0400

    Update admin.py

--- smart_manager/admin.py
+++ smart_manager/admin.py
@@ -3,7 +3,7 @@ from smart_manager.models import SmartManager
 
 
 class SmartManagerAdmin(admin.ModelAdmin):
-    fields = ('name', 'smart_manager_class', 'primary_obj_type', 'primary_obj_id')
+    list_display = ('name', 'smart_manager_class', 'primary_obj_type', 'primary_obj_id')
 
 
 admin.site.register(SmartManager, SmartManagerAdmin)
