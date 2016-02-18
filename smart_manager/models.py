490d2f667939e2a30a2873efa53939c6d9ce4a8e
Wes Okes <wes.okes@gmail.com>
2016-02-18 15:08:57 -0500

    fix generic key path

--- smart_manager/models.py
+++ smart_manager/models.py
@@ -1,7 +1,7 @@
 import inspect
 import traceback
 
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models, transaction
@@ -31,7 +31,7 @@ class SmartManager(models.Model):
     # The primary object that this smart manager manages
     primary_obj_type = models.ForeignKey(ContentType, null=True)
     primary_obj_id = models.PositiveIntegerField(default=0)
-    primary_obj = generic.GenericForeignKey('primary_obj_type', 'primary_obj_id')
+    primary_obj = GenericForeignKey('primary_obj_type', 'primary_obj_id')
 
     # The template of the model(s) being managed
     template = JSONField()
@@ -92,7 +92,7 @@ class SmartManagerObject(models.Model):
     # The generic foreign key to the object
     model_obj_type = models.ForeignKey(ContentType)
     model_obj_id = models.PositiveIntegerField()
-    model_obj = generic.GenericForeignKey('model_obj_type', 'model_obj_id', for_concrete_model=False)
+    model_obj = GenericForeignKey('model_obj_type', 'model_obj_id', for_concrete_model=False)
 
     objects = ManagerUtilsManager()
 
